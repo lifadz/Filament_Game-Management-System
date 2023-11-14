d804c58a37df2f7f67be1cba97533bdd981d1ee4
lifadz <fadmuh12@gmail.com>
2023-11-14 23:34:55 +0700

    Sori baru ngumpul bang

---
+++ app/Filament/Resources/DeveloperResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DeveloperResource\Pages;
+use App\Filament\Resources\DeveloperResource\RelationManagers;
+use App\Models\Developer;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DeveloperResource extends Resource
+{
+    protected static ?string $model = Developer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('developer_name'),
+                DatePicker::make('founded_date'),
+                TextInput::make('country')
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('developer_name')
+                    ->sortable(),
+                TextColumn::make('founded_date'),
+                TextColumn::make('country')
+                
+                
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDevelopers::route('/'),
+            // 'create' => Pages\CreateDeveloper::route('/create'),
+            // 'edit' => Pages\EditDeveloper::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Developer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Developer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['developer_name','founded_date','country'];
+
+    public $timestamps = false;
+
+}
