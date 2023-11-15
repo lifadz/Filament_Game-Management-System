a716ca08ff0cc3473512896a73499c82c83e4ccd
Fadzli Muhammad <65546947+lifadz@users.noreply.github.com>
2023-11-15 10:29:19 +0700

    Update GenreResource.php

--- app/Filament/Resources/GenreResource.php
+++ app/Filament/Resources/GenreResource.php
@@ -19,7 +19,7 @@ class GenreResource extends Resource
 {
     protected static ?string $model = Genre::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-rocket-launch';
 
     public static function form(Form $form): Form
     {
